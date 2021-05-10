b4dcf1196b308d837eb9ee4b0d68ab9e89080e3a
Conover (US) <beihua.c.conover@boeing.com>
2021-05-09 19:22:51 -0700

    homework3

---
+++ homework3/SecondWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace homework3 {
+  /// <summary>
+  /// Interaction logic for SecondWindow.xaml
+  /// </summary>
+  public partial class SecondWindow : Window {
+    public SecondWindow()
+    {
+      InitializeComponent();
+
+      var users = new List<Models.User>();
+
+      users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+      users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+      users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+      uxList.ItemsSource = users;
+    }
+
+    private void OnColumnHeaderName_Click(object sender, RoutedEventArgs e)
+    {
+      _sortColumnPerColumnHeader("Name");
+    }
+
+    private void OnColumnHeaderPassword_Click(object sender, RoutedEventArgs e)
+    {
+      _sortColumnPerColumnHeader("Password");
+    }
+    
+    private void _sortColumnPerColumnHeader(string headerName)
+    {
+      CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+
+      view.SortDescriptions.Clear();
+
+      view.SortDescriptions.Add(new SortDescription(headerName, ListSortDirection.Ascending));
+    }
+  }
+}

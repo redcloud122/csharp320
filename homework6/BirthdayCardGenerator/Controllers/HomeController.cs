31b50ea0e72de7459d23cf82d855b65b3deae675
Conover (US) <beihua.c.conover@boeing.com>
2021-06-15 19:07:03 -0700

    homework6

---
+++ homework6/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BirthdayCardGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCardGenerator.Controllers {
+  public class HomeController : Controller {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    // Receive the data coming from the browser
+    [HttpPost]
+    public IActionResult Index(Models.BirthdayCard birthdayCard)
+    {
+      if (ModelState.IsValid) {
+        return View("Confirmation", birthdayCard);
+      }
+      else {
+        return View(birthdayCard);
+      }
+    }
+
+  }
+}
